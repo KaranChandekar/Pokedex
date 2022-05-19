345e03284a1fd3adf28f2a3b94c0c3aae72811d1
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-19 20:34:23 +0530

    color object added

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const poke_container = document.getElementById("poke-container");
 const pokemon_count = 150;
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
