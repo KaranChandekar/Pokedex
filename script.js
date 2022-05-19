568fdc3e1d9eb52b42280f53f42c272c5e2fe5ec
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-19 20:35:34 +0530

    function getPokemon

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ const fetchPokemons = async () => {
     await getPokemon(i);
   }
 };
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  createPokemonCard(data);
+};
